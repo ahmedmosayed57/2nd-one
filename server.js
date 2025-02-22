8073c5fdf182cc52270e3d3132460b70e5388206
ahmedmosayed57 <mosayedsh57@gmail.com>
2025-02-22 13:06:25 +0200

    created my new project

---
+++ server.js
@@ -0,0 +1,35 @@
+const express  = require('express');
+const cors = require('cors');
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+
+
+
+app.get('/', (req,res)=>{
+    res.send('Home')
+})
+
+
+
+app.get('/users/:name', (req,res)=>{
+    res.send('users')
+})
+
+app.get('/posts' ,(req,res)=>{
+    res.send('iam here')
+})
+
+
+
+
+app.listen(3000, ()=>{
+    console.log('Server Is runing..........')
+})
+
+
+module.exports = {app}
